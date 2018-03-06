a0bed8429aa575912c5bb55ecd491e297b612a49
BenjaminPMLovegood <benjaminpmlee@gmail.com>
2018-03-06 21:44:01 +0800

    add base64 module

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,16 +1,11 @@
 using System;
+using GeminiLab.Core2;
 using GeminiLab.Core2.ML.Json;
 
 namespace TestConsole {
     class Program {
         public static void Main(string[] args) {
-            var str = Console.In.ReadToEnd();
-
-            var jsonValue = JsonParser.Parse(str);
-            Console.WriteLine(jsonValue.ToString());
-            Console.WriteLine(jsonValue.ToStringMinimized());
-            Console.WriteLine(jsonValue.ToStringForNetwork());
-            Console.WriteLine(jsonValue.ToStringPrettified());
+            Console.WriteLine("6Iuf5Yip5Zu95a6255Sf5q275Lul".DecodeBase64EncodedString());
         }
     }
 }
